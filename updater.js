18c0a03342341ef618b502e45a1f01117c461b12
Francisco <blindpuppeteer@gmail.com>
2022-01-04 23:20:00 -0300

    Initial init

---
+++ updater.js
@@ -0,0 +1,24 @@
+const baseUrl = 'https://raw.githubusercontent.com/acookienot/bitburner/master/';
+const url = path => `${baseUrl}${path}`;
+
+async function getFile(ns, file) {
+    await ns.wget(url(file), file);
+}
+
+export async function main(ns) {
+    let files = [
+        'test.js',
+        /*'utils.js',
+        'Inject.js',
+        'Server.js',
+        'Crack.js',
+        'grow.js',
+        'hack.js',
+        'weaken.js',*/
+        'updater.js'
+    ];
+
+    for (let file of files) {
+        await getFile(ns, file);
+    }
+}
